64d90e1641506389bdb960ea203e773e1006eb34
Martin Vejnár <avakar@ratatanek.cz>
2015-04-18 13:47:13 +0200

    up the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='crater',
-    version='0.1.0',
+    version='0.1.1',
 
     description='A dependency management system',
     author='Martin Vejnár',
