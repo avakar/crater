08fb59f8f0875a7d742df8d1e77090f3a64657d6
Martin Vejnár <avakar@ratatanek.cz>
2015-10-03 10:42:35 +0200

    Add pytoml dependency.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='crater',
-    version='0.1.3',
+    version='0.1.4',
 
     description='A dependency management system',
     author='Martin Vejnár',
@@ -14,6 +14,7 @@ setup(
     license='MIT',
 
     packages=['crater'],
+    install_requires=['pytoml'],
     entry_points = {
         'console_scripts': [
             'crater = crater.crater:main',
